5a8c8277306c1425afe83e6cf1ffd7c3b13b80d4
ginogiorgi <ginorubengiorgi@gmail.com>
2023-05-28 00:58:19 -0300

    Clase 22 del Curso de Asincronismo con JavaScript de Platzi

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,8 +2,8 @@ const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCzzsHtfh30tPGd
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '7d1c20b94fmsh123c1042392efc6p10da33jsn0b5566fe3a89',
-		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+		'X-RapidAPI-Host':'youtube-v31.p.rapidapi.com',
+		'X-RapidAPI-Key':'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256',
 	}
 };
 const content = null || document.getElementById("content");
@@ -22,7 +22,7 @@ async function fetchData(urlApi) {
         ${videos.items.map(video => `
         <div class="group relative">
             <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+                <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
             </div>
             <div class="mt-4 flex justify-between">
                 <h3 class="text-sm text-gray-700">
@@ -33,7 +33,8 @@ async function fetchData(urlApi) {
         </div>
         `).slice(0,4).join("")}
         `;
-    } catch {
-
-    }
+        content.innerHTML = view;
+    } catch (error){
+        console.log(error);
+    };
 })();
